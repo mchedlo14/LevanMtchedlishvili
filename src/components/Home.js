5d2d521087921743c6f932dabbbfabda57c66348
levan mchedlishvili <golocogo4@gmail.com>
2022-11-15 15:56:38 +0400

    fix design bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,8 +20,10 @@ const Home = () => {
         <div data-aos="zoom-in" className='information-container'>
             <p className='job'>Front End Developer</p>
             <p className={`about-me-${theme.theme}`}>Hello,  my name is Levan Mtchedlishvili</p>
-            <p className='details-about-me'>I'm a Front End Developer.Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website.
-            This is my portfolio where you can see: projects, cv, contact information, my social accounts. Welcome!</p>
+            <p className='details-about-me'>Hey my name is Levan. I'm a Front End Developer.Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website.
+
+            This is my portfolio where you can see: projects, cv, contact information, my social accounts.
+            A few things about me: I'm creative because I can solve a problem in different ways.I am always energetic and eager to learn new skills.One of my best plus is correct time management.</p>
             <div className='buttons-container'>
                 <a href={require("../cv/levan_mtchedlishvili.pdf")} download="Levan Mtchedlishvili CV">
                     <button className='cv-btn'>Download CV</button>
