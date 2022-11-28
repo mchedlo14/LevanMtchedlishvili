8fc3b7165720c7723cf3d438b2049aaa73ccc6f6
mchedlo14 <golocogo4@gmail.com>
2022-11-28 04:15:24 -0800

    add new navbar v1.0.1

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  {Navbar,Nav,Container} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import SelectTheme from './SelectTheme';
+import '../assets/NavbarComp.css'
+import { Menu, Item } from "burger-menu";
+import 'burger-menu/lib/index.css';
+
 
 
 const NavbarComp = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      {" "}
+    <div className='navbar-wrapper'>
+      {/* {" "}
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link to='/' style={{ textDecoration: 'none' }}>
@@ -29,7 +34,26 @@ const NavbarComp = () => {
           <SelectTheme />
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </Navbar> */}
+    <div className='navbar-container'>
+      <Link to='/'  style={{ textDecoration: 'none' }}>
+        <p>Levan Mtchedlishvili</p>
+      </Link>
+      <img src='/images/menu.svg' alt='burger menu' className='burger-icon' onClick={() => setIsOpen(!isOpen)}/>
+      <Menu className="burger-menu" isOpen={isOpen} selectedKey={'entry'} onClose={() => setIsOpen(false)}>
+        <Link to='/'>
+          <Item itemKey={'manage'} text={'Home'}></Item>
+        </Link>
+        <Link to='/about'>
+          <Item itemKey={'manage'} text={'About'}></Item>
+        </Link>
+        <Link to='/timeline'>
+          <Item itemKey={'user'} text={'Timeline'}></Item>
+        </Link>
+        {/* <SelectTheme /> */}
+      </Menu>
+    </div>
+    
     </div>
   );
 }
