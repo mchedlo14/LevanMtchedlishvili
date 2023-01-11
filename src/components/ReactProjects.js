d74d90ec1b6272021c6a562e92254c0dd2f0656f
mchedlo14 <golocogo4@gmail.com>
2023-01-11 05:35:21 -0800

    added crypto web

--- src/components/ReactProjects.js
+++ src/components/ReactProjects.js
@@ -25,69 +25,76 @@ const ReactProjects = () => {
             </a>
         </div>
         <div data-aos="fade-right" className='project-container'>
+            <h2 className={`project-title-${theme.theme}`}>Crypto Web</h2>
+            <p className={`project-description-${theme.theme}`}>This is a Crypto Landing page also you can find information about your favorite crypto coin.</p>
+            <a href='https://cozy-tartufo-794a95.netlify.app' target="_blank" rel="noreferrer">
+                <button className='project-button'>View Project</button>
+            </a>
+        </div>
+        <div data-aos="fade-left" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Crypto Currency</h2>
             <p className={`project-description-${theme.theme}`}>This is a website where you can find everything about the price of cryptocurrencies (price, percentage increase, percentage decrease).</p>
             <a href='https://currencydb.netlify.app' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-left" className='project-container'>
+        <div data-aos="fade-right" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>User App </h2>
             <p className={`project-description-${theme.theme}`}>This is a mini fullstack website where you can see the information about users.</p>
             <a href='https://github.com/mchedlo14/user-app' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-right" className='project-container'>
+        <div data-aos="fade-left" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>ToDo App</h2>
             <p className={`project-description-${theme.theme}`}>ToDo List App is a kind of app that is generally used to maintain our day-to-day tasks or list everything that we have to do.</p>
             <a href='https://tdgoals.netlify.app' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-left" className='project-container'>
+        <div data-aos="fade-right" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Pc Shop</h2>
             <p className={`project-description-${theme.theme}`}>I used API in this project. In this project You can add your computer or laptop to this project and finally see the created list.</p>
             <a href='https://funny-bubblegum-25ca41.netlify.app' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-right" className='project-container'>
+        <div data-aos="fade-left" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Chess Knight</h2>
             <p className={`project-description-${theme.theme}`}>In this project you can registration to the chess competition.</p>
             <a href='https://redberrychess.netlify.app' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-left" className='project-container'>
+        <div data-aos="fade-right" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Advice App</h2>
             <p className={`project-description-${theme.theme}`}>do you have a problem? Advice generator will help you.Maybe find a solution.</p>
             <a href='https://uncleroland.netlify.app/' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-right" className='project-container'>
+        <div data-aos="fade-left" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Github Users</h2>
             <p className={`project-description-${theme.theme}`}>In this project you can find github users.</p>
             <a href='https://gusers.netlify.app/' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-left" className='project-container'>
+        <div data-aos="fade-right" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Weather App</h2>
             <p className={`project-description-${theme.theme}`}>In this project, you can search the country and see the weather temperature.</p>
             <a href='https://weatherappdb.netlify.app/' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-right" className='project-container'>
+        <div data-aos="fade-left" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Movie App</h2>
             <p className={`project-description-${theme.theme}`}>I used API in this project. You can see the list of movies, also You can search for movies.</p>
             <a href='https://movieappimdb.netlify.app' target="_blank" rel="noreferrer">
                 <button className='project-button'>View Project</button>
             </a>
         </div>
-        <div data-aos="fade-left" className='project-container'>
+        <div data-aos="fade-right" className='project-container'>
             <h2 className={`project-title-${theme.theme}`}>Fifa World Cup</h2>
             <p className={`project-description-${theme.theme}`}>In this project you can see today's matches and live matches.</p>
             <a href='https://github.com/mchedlo14/Fifa-World-Cup-Qatar' target="_blank" rel="noreferrer">
