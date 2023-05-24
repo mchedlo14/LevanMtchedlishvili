0e6a3967832aed15cd2944b5312a2911997a3acb
mchedlo14 <golocogo4@gmail.com>
2023-05-24 13:49:20 +0400

    changed timeline component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -11,53 +11,80 @@ const Timeline = () => {
 	<div class="timeline-items">
 		<div class="timeline-item">
 			<div class="timeline-dot"></div>
-			<div class="timeline-date">2019</div>
+			<div class="timeline-date">2023 May</div>
 			<div class="timeline-content">
-				<h3>Python basic</h3>
-				<p>In 2019, I started learning the basics of Python</p>
+				<h3>Shopify</h3>
+				<p>I started learning the shopify platform and template language liquid</p>
 			</div>
 		</div>
 		<div class="timeline-item">
 			<div class="timeline-dot"></div>
-			<div class="timeline-date">2020</div>
+			<div class="timeline-date">2023 Feb</div>
 			<div class="timeline-content">
-				<h3>Python Advanced</h3>
-				<p>After the beginnings of basics, I learned the advanced level of Python.</p>
+				<h3>Ant Design</h3>
+				<p>I learned Ant Design which is popular design system for developing enterprise products.</p>
 			</div>
 		</div>
 		<div class="timeline-item">
 			<div class="timeline-dot"></div>
-			<div class="timeline-date">2020</div>
+			<div class="timeline-date">2023 Jan</div>
 			<div class="timeline-content">
-				<h3>HTML CSS SCSS JS</h3>
-				<p>After that I started learning the web development And I learned HTML,CSS,JS</p>
+				<h3>Node Js</h3>
+				<p>I have acquired a solid foundation in Nodejs</p>
 			</div>
 		</div>
 		<div class="timeline-item">
 			<div class="timeline-dot"></div>
-			<div class="timeline-date">2021 Dec</div>
+			<div class="timeline-date">2022 Dec</div>
 			<div class="timeline-content">
 				<h3>React</h3>
-				<p>Then I continued to learn Js library React</p>
+				<p>I learned the react perfectly.</p>
 			</div>
 		</div>
 		<div class="timeline-item">
 			<div class="timeline-dot"></div>
-			<div class="timeline-date">2022 Jan</div>
+			<div class="timeline-date">2021 Jan</div>
 			<div class="timeline-content">
-				<h3>UI/UX</h3>
-				<p>I learned ui/ux design at a basic level, because I think it is necessary for a web developer.</p>
+				<h3>React Basics</h3>
+				<p>I learned React and its Main principles.</p>
 			</div>
 		</div>
-    <div class="timeline-item">
-			<div class="timeline-dot"></div>
-			<div class="timeline-date">2022</div>
-			<div class="timeline-content">
-				<h3>MUI Bootstrap</h3>
-				<p>Then I learned Mui and bootstrap</p>
+			<div class="timeline-item">
+				<div class="timeline-dot"></div>
+				<div class="timeline-date">2021 Feb</div>
+				<div class="timeline-content">
+					<h3>UI/UX</h3>
+					<p>I learned UI/UX design at a basic level, because I think it is necessary for a web developer.</p>
+				</div>
+			</div>
+
+			<div class="timeline-item">
+				<div class="timeline-dot"></div>
+				<div class="timeline-date">2021 Sep</div>
+				<div class="timeline-content">
+					<h3>Linux Basics</h3>
+					<p>I learned basics of linux</p>
+				</div>
+			</div>
+			<div class="timeline-item">
+				<div class="timeline-dot"></div>
+				<div class="timeline-date">2020 Sep</div>
+				<div class="timeline-content">
+					<h3>Python Web Scrapping</h3>
+					<p>I learned beautifulsoup4</p>
+				</div>
+			</div>
+			<div class="timeline-item">
+				<div class="timeline-dot"></div>
+				<div class="timeline-date">2019 Sep</div>
+				<div class="timeline-content">
+					<h3>Python Basics</h3>
+					<p>I started learning python basics.</p>
+				</div>
 			</div>
 		</div>
-	</div>
+		
+		
 </section>
     </>
   )
